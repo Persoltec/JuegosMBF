dce7e0008e11b8f16b06bf35ca0ed09e6fc34d62
Jesus <persoltec.com@gmail.com>
2019-09-02 13:44:07 -0500

    ---

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,6 @@ module.exports = {
 				name: 'pages',
 			},
 		},
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				path: `${__dirname}/src/img`,
-				name: 'images',
-			},
-		},
 		'gatsby-plugin-sharp',
 		'gatsby-transformer-sharp',
 		{
